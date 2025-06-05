1adcac2064893e4eeba06233d4650e8745829b6f
autofollower350 <followerauto350@gmail.com>
2025-06-05 14:17:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(
 
 
 # Bot credentials from environment variables (Render compatible)
-API_ID = int(os.environ.get("API_ID", 28590286)
+API_ID = int(os.environ.get("API_ID", "28590286")
 API_HASH = os.environ.get("API_HASH", "6a68cc6b41219dc57b7a52914032f92f")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "7412939071:AAFk6_MJziu22UnZOv9crH-HcwLDqzQ1SVg")
 
